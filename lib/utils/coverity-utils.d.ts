64d64e9d6923efc3f24f1dbcdd64d753c757e9f6
James Croall <jcroall@pobox.com>
2022-03-21 11:59:09 +0000

    Done

--- lib/utils/coverity-utils.d.ts
+++
@@ -1,5 +0,0 @@
-import { CoverityIssueOccurrence } from "../models/coverity-json-v7-schema";
-export declare function coverityIsPresent(existingMessage: string): boolean;
-export declare function coverityCreateNoLongerPresentMessage(existingMessage: string): string;
-export declare function coverityCreateReviewCommentMessage(issue: CoverityIssueOccurrence): string;
-export declare function coverityCreateIssueCommentMessage(issue: CoverityIssueOccurrence): string;
--- lib/utils/diffmap.d.ts
+++
@@ -1,5 +0,0 @@
-export declare type DiffMap = Map<string, Hunk[]>;
-export interface Hunk {
-    firstLine: number;
-    lastLine: number;
-}
