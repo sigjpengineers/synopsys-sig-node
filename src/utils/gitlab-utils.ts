766ad8c1d44638f2aee8aa61e6eac1b9f438dc3f
James Croall <jcroall@pobox.com>
2022-03-26 14:57:58 +0000

    Add Coverity and GitHub

--- src/utils/gitlab-utils.ts
+++ src/utils/gitlab-utils.ts
@@ -97,10 +97,10 @@ export async function gitlabUpdateNote(gitlab_url: string, gitlab_token: string,
                                        discussion_id: number, note_id: number, body: string): Promise<void> {
     const api = new Gitlab({ token: gitlab_token })
 
-    logger.debug(`Update discussion for merge request #${merge_request_iid} in project #${project_id}`)
+    logger.debug(`Update discussion #${discussion_id} note #${note_id} for merge request #${merge_request_iid} in project #${project_id} `)
+    logger.debug(`new body is: ${body}`)
 
-    await api.MergeRequestDiscussions.editNote(project_id, merge_request_iid, discussion_id, note_id,
-        { body: body })
+    await api.MergeRequestDiscussions.editNote(project_id, merge_request_iid, discussion_id, note_id, { body: body })
 }
 
 export async function gitlabCreateDiscussionWithoutPosition(gitlab_url: string, gitlab_token: string, project_id: string, merge_request_iid: number,
