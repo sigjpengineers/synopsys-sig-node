ce56aad3bf9eb13c9c0da9a0ead70a0e3a348e71
James Croall <jcroall@pobox.com>
2022-03-30 23:06:27 +0000

    Introduce Polaris code

---
+++ src/polaris/cli/PolarisRunner.ts
@@ -0,0 +1,79 @@
+import PolarisRunResult from "../model/PolarisRunResult";
+const fs = require('fs');
+const fse = require('fs-extra');
+const path = require('path');
+const urlParser = require('url');
+import PolarisInstall from "../model/PolarisInstall";
+import PolarisConnection from "../model/PolarisConnection";
+import {logger} from "../../SIGLogger";
+import {exec} from "@actions/exec";
+
+export default class PolarisRunner {
+    log: any;
+    constructor(log:any) {
+        this.log = log;
+    }
+
+    async execute_cli(connection: PolarisConnection, polaris_install: PolarisInstall, cwd: string, build_command: string):Promise<PolarisRunResult> {
+        var env: any = process.env;
+        
+        env["POLARIS_SERVER_URL"] = connection.url;
+        env["POLARIS_ACCESS_TOKEN"] = connection.token;
+
+        if (connection.proxy != undefined) {
+            var proxyOpts = urlParser.parse(connection.proxy.proxy_url);
+            if (connection.proxy.proxy_username && connection.proxy.proxy_password) {
+                proxyOpts.auth = connection.proxy.proxy_username + ":" + connection.proxy.proxy_password;
+            }
+            env["HTTPS_PROXY"] = urlParser.format(proxyOpts);
+        }
+
+        if ("POLARIS_HOME" in env) {
+            this.log.info("A POLARIS_HOME exists, will not attempt to override.")    
+        } else {
+            var override_home = polaris_install.polaris_home;
+            if (!fs.existsSync(override_home)) {
+                this.log.info("Creating plugin Polaris home: " + override_home)
+                fse.ensureDirSync(override_home);
+            } else {
+                this.log.debug("Polaris home already exists, it will not be created.")
+            }
+            
+            if (fs.existsSync(override_home)) {
+                this.log.info("Set POLARIS_HOME to directory: " + override_home)
+                env["POLARIS_HOME"] = override_home
+            } else {
+                this.log.error("Unable to create a POLARIS_HOME and env variable was not set. Will not override. Try creating POLARIS_HOME on the agent or ensuring agent has access.")
+            }
+        }
+
+        logger.info(`Executing ${polaris_install.polaris_executable} with line=${build_command}`)
+
+        /*
+        await exec(polaris_install.polaris_executable,
+            ['-jar', detectPath].concat(detectArguments), { ignoreReturnCode: true })
+         */
+
+        /*
+        let exe = tl.tool(polaris_install.polaris_executable);
+        exe.line(build_command);
+    
+        var return_code =  await exe.exec(<tr.IExecOptions>{
+            cwd: cwd,
+            env: env
+        });
+
+         */
+
+        var return_code = 1
+
+        var synopsysFolder = path.join(cwd, ".synopsys");
+        var polarisFolder = path.join(synopsysFolder, "polaris");
+        var scanJsonFile = path.join(polarisFolder, "cli-scan.json");
+
+        delete process.env["HTTPS_PROXY"];
+
+        return new PolarisRunResult(return_code, scanJsonFile);
+    }
+}
+
---
+++ src/polaris/service/PolarisService.ts
@@ -0,0 +1,169 @@
+
+import * as os from 'os';
+var ProxyAgent = require("proxy-agent");
+const HttpsProxyAgent = require("https-proxy-agent");
+const url = require('url');
+const Axios = require('axios');
+const moment = require("moment");
+const CancelToken = Axios.CancelToken;
+const fs = require('fs');
+const json_path = require('jsonpath');
+const debug = require('debug');
+import PolarisConnection from "../model/PolarisConnection";
+
+export default class PolarisService {
+    log: any;
+    polaris_url: string;
+    access_token: string;
+    bearer_token: string | null;
+    headers: any | null;
+    axios: any;
+    constructor(log:any, connection: PolarisConnection) {
+        if (connection.url.endsWith("/") || connection.url.endsWith("\\")) {
+            this.polaris_url = connection.url.slice(0, -1);
+        } else {
+            this.polaris_url = connection.url;
+        }
+        
+        this.access_token = connection.token;
+        this.bearer_token = null;
+        this.headers = null;
+        this.log = log;
+        
+
+        if (connection.proxy != undefined) {
+            log.info(`Using Proxy URL: ${connection.proxy.proxy_url}`)
+            var proxyOpts = url.parse(connection.proxy.proxy_url);
+
+            var proxyConfig :any = { 
+                host: proxyOpts.hostname, 
+                port: proxyOpts.port 
+            };
+
+            if (connection.proxy.proxy_username && connection.proxy.proxy_password) {
+                log.info("Using configured proxy credentials.")
+                proxyConfig.auth = connection.proxy.proxy_username + ":" + connection.proxy.proxy_password;
+            }
+
+            const httpsAgent = new HttpsProxyAgent(proxyConfig)
+            this.axios = Axios.create({httpsAgent});
+        } else {
+            this.axios = Axios.create();
+        }
+    }
+
+    async authenticate() {
+        this.log.info("Authenticating with Polaris Software Integrity Platform.")
+        debug.enable('https-proxy-agent');
+        this.bearer_token = await this.fetch_bearer_token();
+        debug.disable();
+        this.headers = {
+            Authorization: `Bearer ${this.bearer_token}`
+        }
+    }
+
+    async get_job(job_status_url: string) {
+        return await this.axios.get(job_status_url, {
+            headers: this.headers
+        });
+    }
+
+    fetch_bearer_token():Promise<string> {
+        // this is a workaround for https://github.com/TooTallNate/node-https-proxy-agent/issues/102
+        //basically NodeJS thinks all event loops are closed, this ensures the event look hasn't closed. 
+        // TODO: Need to switch to a new http library that doesn't suffer from this bug.
+        //Basically we need to reject the promise ourselves 
+        const resultPromise = new Promise<string>((resolve, reject) => {
+            const timeout = 10000
+            setTimeout(() => { reject(new Error(`Failed to authenticate with Polaris Software Integrity Platform. This may be a problem with your url, proxy setup or network.`))}, timeout);
+
+            var authenticateUrl = this.polaris_url + "/api/auth/authenticate";
+    
+            try {
+                this.axios.post(authenticateUrl, "accesstoken=" + this.access_token, {timeout: 10000, headers: {'Content-Type': 'application/x-www-form-urlencoded'}}).then((authResponse:any) => {
+                    if (authResponse.data.jwt) {
+                        this.log.info("Succesfully authenticated, saving bearer token.")
+                        resolve(authResponse.data.jwt);
+                    } else {
+                        this.log.error(`Failed to authenticate with Polaris Software Integrity Platform, no bearer token received.`)
+                        reject(new Error(`Failed to authenticate with Polaris Software Integrity Platform. Status: ${authResponse.status} Reason: ${authResponse.statusText}`))
+                    }
+                }).catch((e:any) => {
+                    this.log.error(`Unable to authenticate with Polaris Software Integrity Platform at url: ${authenticateUrl}`);
+                    this.log.error(`This may be a problem with your Polaris Software Integrity Platform url, proxy setup or network.`);
+                    reject(e);
+                })
+            } catch (e) {
+                this.log.error(`Unable to authenticate with Polaris Software Integrity Platform at url: ${authenticateUrl}`);
+                this.log.error(`This may be a problem with your Polaris Software Integrity Platform url, proxy setup or network.`);
+                reject(e);
+            }
+        })
+        return resultPromise;
+    }
+
+    async fetch_cli_modified_date(url: string): Promise<any> { //return type should be a moment
+        var token = CancelToken.source();
+        var self = this;
+        self.log.debug("Fetching cli modified date from: " + url);
+        return new Promise((resolve, reject) => {
+            this.axios({
+                url: url,
+                method: 'GET',
+                responseType: 'stream', // important, let's us cancel the after we get the headers.
+                cancelToken: token.token
+            }).then(function (response: any) {
+                var lastModifiedText = response.headers['last-modified'];
+                self.log.debug("Last Modified Header: " + lastModifiedText);
+                var lastModifiedDate = moment(lastModifiedText);
+                self.log.debug("Last Modified Date: " + lastModifiedDate.format());
+                token.cancel();
+                resolve(lastModifiedDate);
+            }).catch(function (error: any) {
+                reject(error);
+            });
+        });
+    }
+
+    async fetch_issue_data(url: string): Promise<any> {
+        return await this.axios.get(url, {
+            headers: this.headers
+        });
+    }
+
+    async fetch_organization_name(): Promise<string | null> {
+        var target = this.polaris_url + "/api/auth/contexts";
+        var result = await this.axios({
+            url: target,
+            method: 'GET',
+            responseType: 'json',
+            headers: this.headers,
+        });
+        var organization_names = json_path.query(result.data, "$.data[*].attributes.organizationname");
+        if (organization_names.length > 0) {
+            return organization_names[0];
+        } else {
+            return null;
+        }
+    }
+    
+    async download_cli(url: string, file: string) {
+        this.log.debug("Downloading cli from: " + url);
+        this.log.debug("Downloading cli to: " + file);
+
+        const writer = fs.createWriteStream(file);
+    
+        const response = await this.axios({
+            url,
+            method: 'GET',
+            responseType: 'stream'
+        });
+    
+        response.data.pipe(writer);
+    
+        return new Promise((resolve, reject) => {
+            writer.on('finish', resolve);
+            writer.on('error', reject)
+        });
+    }
+}
