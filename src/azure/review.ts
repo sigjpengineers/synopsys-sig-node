83b6a4fb9cf7704ce68abeb1b2fc29c5a5eb7f27
James Croall <jcroall@pobox.com>
2022-04-09 01:28:20 +0000

    Introduce Polaris code

--- src/azure/review.ts
+++ src/azure/review.ts
@@ -36,6 +36,32 @@ export async function azUpdateComment(git_agent: IGitApi, repo_id: string, pull_
     return true
 }
 export async function azCreateReviewComment(git_agent: IGitApi, repo_id: string, pull_id: number,
+                                                 path: string, line: number,
+                                                 comment_body: string): Promise<boolean> {
+    let comment: Comment = <Comment>{}
+    comment.content = comment_body
+    comment.parentCommentId = 0
+    comment.commentType = 1
+
+    let thread: GitPullRequestCommentThread = <GitPullRequestCommentThread>{}
+    thread.threadContext = <CommentThreadContext>{}
+    thread.threadContext.filePath = "/" + path
+    thread.threadContext.rightFileStart = <CommentPosition>{}
+    thread.threadContext.rightFileStart.line = line
+    thread.threadContext.rightFileStart.offset = 1
+    thread.threadContext.rightFileEnd = <CommentPosition>{}
+    thread.threadContext.rightFileEnd.line = line
+    thread.threadContext.rightFileEnd.offset = 1
+    thread.status = 1 // Active
+
+    thread.comments = [ comment ]
+
+    let new_thread = await git_agent.createThread(thread, repo_id, pull_id)
+
+    return true
+}
+
+export async function azCreateSigmaReviewComment(git_agent: IGitApi, repo_id: string, pull_id: number,
                                             issue: SigmaIssueOccurrence, comment_body: string): Promise<boolean> {
     let comment: Comment = <Comment>{}
     comment.content = comment_body
